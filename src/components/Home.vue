68c0465a6a646f15b4278f84b0be4d921cbdd0ef
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2023-12-08 11:30:25 +0100

    Feat: get data from api when token is right

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,19 @@
 <script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+fetch("http://localhost:3000/api/v1/shoes", {
+  headers: {
+    "Authorization": "Bearer " + localStorage.getItem('token'),
+  },  
+}).then((result) => {
+}).then((json) => {
+}).catch((err) => {
+    router.push('/');
+});
+
+
+
 </script>
 <template>Overview</template>
 <style scoped></style>

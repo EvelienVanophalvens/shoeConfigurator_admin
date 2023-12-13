240796146d1a8b788bc15220e6d48788efd8073a
Yorick Devleeschouwer <yorick.devleeschouwer@gmail.com>
2023-12-13 12:40:54 +0100

    Feat: shoe cards + fetch

---
+++ src/components/subcomponents/Shoe.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+<template>
+    <p>Shoe type: </p>
+    <p>Size: </p>
+    <p>Material: </p>
+    <h3>Colors</h3>
+    <p>Laces: </p>
+    <p>Outsole: </p>
+    <p>Midsole:</p>
+    <p>Outer material: </p>
+    <p>Inner material: </p>
+</template>
+<style scoped>
+</style>

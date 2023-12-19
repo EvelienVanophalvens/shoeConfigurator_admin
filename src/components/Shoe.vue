395aafd3c03296095f344f0d7e9017e7b11bc867
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2023-12-19 11:31:31 +0100

    Feat: search by ordernumber

--- src/components/Shoe.vue
+++ src/components/Shoe.vue
@@ -3,6 +3,7 @@ import { useRouter } from 'vue-router'
 import { ref } from 'vue'
 
 const data = ref([]);
+
 const urlParams = new URLSearchParams(window.location.search);
 const router = useRouter()
 let newStage = ref("");
@@ -113,13 +114,16 @@ const removeShoe = (id) => {
             console.error('Error:', error);
         });
 }
+
+
+
 </script>
 <template>
     <svg @click="goBack" class="icon icon--closeIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
     <div class="container">
         <div class="container__item">
-            <h1>{{ data.shoeName }}</h1>
-            <h2>Size: {{ data.shoeSize }}</h2>
+            <h1>{{ data.shoeName}}</h1>
+            <h2>Size: {{ data.shoeSize  }}</h2>
             <p>Material: {{ data.innerMaterial }}</p>
             <h3>Colors:</h3>
             <ul class="container__item__list">
